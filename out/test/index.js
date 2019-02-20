3c51535f530b061b37c69c13e96c82db9bb80946
道化師 <wraith13@hotmail.co.jp>
2019-02-20 11:35:23 +0900

    🛠 test ： テスト項目追加

--- out/test/index.js
+++ out/test/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var source_1 = require("../source");
 var assert = require("assert");
-var deltaRound = function (value) { return value.toFixed(10); };
+var deltaRound = function (value) {
+    return (-0.00000000001 < value && value < 0.00000000001 ? 0.0 : value).toFixed(10);
+};
 var objectDeltaRound = function (o) {
     //  ⚠ 雑に処理してるから array が普通の object になったりするけどここでは気にしない
     var result = {};
@@ -23,39 +25,33 @@ var objectDeltaRound = function (o) {
 var assertDeepNearEqual = function (result, expect) { return assert.deepEqual(objectDeltaRound(result), objectDeltaRound(expect)); };
 describe('phiColors', function () {
     describe('rgbToHsl()', function () {
-        it("rgbToHsl({r:0.0,g:0.0,b:0.0})", function () { return assert.deepEqual(source_1.phiColors.rgbToHsl({ r: 0.0, g: 0.0, b: 0.0 }), { h: 0, s: 0, l: 0 }); });
-        it("rgbToHsl({r:1.0,g:0.0,b:0.0})", function () { return assert.deepEqual(source_1.phiColors.rgbToHsl({ r: 1.0, g: 0.0, b: 0.0 }), { h: 0, s: 2.0 / 3.0, l: 1.0 / 3.0 }); });
+        it("rgbToHsl({r:0.0,g:0.0,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.0, g: 0.0, b: 0.0 }), { h: 0, s: 0, l: 0 }); });
+        it("rgbToHsl({r:1.0,g:0.0,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 1.0, g: 0.0, b: 0.0 }), { h: 0, s: 2.0 / 3.0, l: 1.0 / 3.0 }); });
         it("rgbToHsl({r:0.0,g:1.0,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.0, g: 1.0, b: 0.0 }), { h: Math.PI * 2.0 / 3.0, s: 2.0 / 3.0, l: 1.0 / 3.0 }); });
         it("rgbToHsl({r:0.0,g:0.0,b:1.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.0, g: 0.0, b: 1.0 }), { h: -Math.PI * 2.0 / 3.0, s: 2.0 / 3.0, l: 1.0 / 3.0 }); });
-        /*
-        console.log("rgbToHsl({r:1.0,g:1.0,b:0.0})", rgbToHsl({r:1.0,g:1.0,b:0.0}));
-        console.log("rgbToHsl({r:1.0,g:1.0,b:1.0})", rgbToHsl({r:1.0,g:1.0,b:1.0}));
-        console.log("rgbToHsl({r:0.5,g:0.5,b:0.5})", rgbToHsl({r:0.5,g:0.5,b:0.5}));
-        console.log("rgbToHsl({r:0.1,g:0.0,b:0.0})", rgbToHsl({r:0.1,g:0.0,b:0.0}));
-        console.log("rgbToHsl({r:0.1,g:0.1,b:0.0})", rgbToHsl({r:0.1,g:0.1,b:0.0}));
-        console.log("rgbToHsl({r:0.9,g:0.0,b:0.0})", rgbToHsl({r:0.9,g:0.0,b:0.0}));
-        console.log("rgbToHsl({r:0.9,g:0.9,b:0.0})", rgbToHsl({r:0.9,g:0.9,b:0.0}));
-        */
+        it("rgbToHsl({r:1.0,g:1.0,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 1.0, g: 1.0, b: 0.0 }), { h: Math.PI / 3.0, s: 2.0 / 3.0, l: 2.0 / 3.0 }); });
+        it("rgbToHsl({r:1.0,g:1.0,b:1.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 1.0, g: 1.0, b: 1.0 }), { h: 0, s: 0.0, l: 1.0 }); });
+        it("rgbToHsl({r:0.5,g:0.5,b:0.5})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.5, g: 0.5, b: 0.5 }), { h: 0, s: 0.0, l: 0.5 }); });
+        it("rgbToHsl({r:0.1,g:0.0,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.1, g: 0.0, b: 0.0 }), { h: 0, s: 0.2 / 3.0, l: 0.1 / 3.0 }); });
+        it("rgbToHsl({r:0.1,g:0.1,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.1, g: 0.1, b: 0.0 }), { h: Math.PI / 3.0, s: 0.2 / 3.0, l: 0.2 / 3.0 }); });
+        it("rgbToHsl({r:0.9,g:0.0,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.9, g: 0.0, b: 0.0 }), { h: 0, s: 0.6, l: 0.3 }); });
+        it("rgbToHsl({r:0.9,g:0.9,b:0.0})", function () { return assertDeepNearEqual(source_1.phiColors.rgbToHsl({ r: 0.9, g: 0.9, b: 0.0 }), { h: Math.PI / 3.0, s: 0.6, l: 0.6 }); });
     });
-    /*
-    describe
-    (
-        'hslToRgb()',
-        () =>
-        {
-            console.log("hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:0.0,g:1.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.0,g:1.0,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:0.0,g:0.0,b:1.0}))", hslToRgb(rgbToHsl({r:0.0,g:0.0,b:1.0})));
-            console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0}))", hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0}))", hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0})));
-            console.log("hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5}))", hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5})));
-            console.log("hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0}))", hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0})));
-            console.log("hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0}))", hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0})));
-        }
-    );
-    */
+    describe('hslToRgb()', function () {
+        return [
+            { r: 0.0, g: 0.0, b: 0.0 },
+            { r: 1.0, g: 0.0, b: 0.0 },
+            { r: 0.0, g: 1.0, b: 0.0 },
+            { r: 0.0, g: 0.0, b: 1.0 },
+            { r: 1.0, g: 1.0, b: 0.0 },
+            { r: 1.0, g: 1.0, b: 1.0 },
+            { r: 0.5, g: 0.5, b: 0.5 },
+            { r: 0.1, g: 0.0, b: 0.0 },
+            { r: 0.1, g: 0.1, b: 0.0 },
+            { r: 0.9, g: 0.0, b: 0.0 },
+            { r: 0.9, g: 0.9, b: 0.0 },
+        ]
+            .forEach(function (i) { return it("hslToRgb(rgbToHsl(" + JSON.stringify(i) + "))", function () { return assertDeepNearEqual(source_1.phiColors.hslToRgb(source_1.phiColors.rgbToHsl(i)), i); }); });
+    });
 });
 //# sourceMappingURL=index.js.map
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,8 @@
 import { phiColors } from "../source";
 import assert = require("assert");
 
-const deltaRound = (value: number) => value.toFixed(10);
+const deltaRound = (value: number) =>
+    (-0.00000000001 < value && value < 0.00000000001 ? 0.0: value).toFixed(10);
 const objectDeltaRound = (o: object): object =>
 {
     //  ⚠ 雑に処理してるから array が普通の object になったりするけどここでは気にしない
@@ -40,40 +41,40 @@ describe
             'rgbToHsl()',
             () =>
             {
-                it(`rgbToHsl({r:0.0,g:0.0,b:0.0})`, () => assert.deepEqual(phiColors.rgbToHsl({r:0.0,g:0.0,b:0.0}), {h: 0, s: 0, l: 0}));
-                it(`rgbToHsl({r:1.0,g:0.0,b:0.0})`, () => assert.deepEqual(phiColors.rgbToHsl({r:1.0,g:0.0,b:0.0}), {h: 0, s: 2.0/3.0, l: 1.0/3.0}));
+                it(`rgbToHsl({r:0.0,g:0.0,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.0,g:0.0,b:0.0}), {h: 0, s: 0, l: 0}));
+                it(`rgbToHsl({r:1.0,g:0.0,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:1.0,g:0.0,b:0.0}), {h: 0, s: 2.0/3.0, l: 1.0/3.0}));
                 it(`rgbToHsl({r:0.0,g:1.0,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.0,g:1.0,b:0.0}), {h: Math.PI*2.0/3.0, s: 2.0/3.0, l: 1.0/3.0}));
                 it(`rgbToHsl({r:0.0,g:0.0,b:1.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.0,g:0.0,b:1.0}), {h: -Math.PI*2.0/3.0, s: 2.0/3.0, l: 1.0/3.0}));
-                /*
-                console.log("rgbToHsl({r:1.0,g:1.0,b:0.0})", rgbToHsl({r:1.0,g:1.0,b:0.0}));
-                console.log("rgbToHsl({r:1.0,g:1.0,b:1.0})", rgbToHsl({r:1.0,g:1.0,b:1.0}));
-                console.log("rgbToHsl({r:0.5,g:0.5,b:0.5})", rgbToHsl({r:0.5,g:0.5,b:0.5}));
-                console.log("rgbToHsl({r:0.1,g:0.0,b:0.0})", rgbToHsl({r:0.1,g:0.0,b:0.0}));
-                console.log("rgbToHsl({r:0.1,g:0.1,b:0.0})", rgbToHsl({r:0.1,g:0.1,b:0.0}));
-                console.log("rgbToHsl({r:0.9,g:0.0,b:0.0})", rgbToHsl({r:0.9,g:0.0,b:0.0}));
-                console.log("rgbToHsl({r:0.9,g:0.9,b:0.0})", rgbToHsl({r:0.9,g:0.9,b:0.0}));
-                */
+                it(`rgbToHsl({r:1.0,g:1.0,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:1.0,g:1.0,b:0.0}), {h: Math.PI/3.0, s: 2.0/3.0, l: 2.0/3.0}));
+                it(`rgbToHsl({r:1.0,g:1.0,b:1.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:1.0,g:1.0,b:1.0}), {h: 0, s: 0.0, l: 1.0}));
+                it(`rgbToHsl({r:0.5,g:0.5,b:0.5})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.5,g:0.5,b:0.5}), {h: 0, s: 0.0, l: 0.5}));
+                it(`rgbToHsl({r:0.1,g:0.0,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.1,g:0.0,b:0.0}), {h: 0, s: 0.2/3.0, l: 0.1/3.0}));
+                it(`rgbToHsl({r:0.1,g:0.1,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.1,g:0.1,b:0.0}), {h: Math.PI/3.0, s: 0.2/3.0, l: 0.2/3.0}));
+                it(`rgbToHsl({r:0.9,g:0.0,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.9,g:0.0,b:0.0}), {h: 0, s: 0.6, l: 0.3}));
+                it(`rgbToHsl({r:0.9,g:0.9,b:0.0})`, () => assertDeepNearEqual(phiColors.rgbToHsl({r:0.9,g:0.9,b:0.0}), {h: Math.PI/3.0, s: 0.6, l: 0.6}));
             }
         );
-        /*
         describe
         (
             'hslToRgb()',
             () =>
-            {
-                console.log("hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:0.0,g:1.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.0,g:1.0,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:0.0,g:0.0,b:1.0}))", hslToRgb(rgbToHsl({r:0.0,g:0.0,b:1.0})));
-                console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0}))", hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0}))", hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0})));
-                console.log("hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5}))", hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5})));
-                console.log("hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0}))", hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0})));
-                console.log("hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0}))", hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0})));
-            }
+                [
+                    {r:0.0,g:0.0,b:0.0},
+                    {r:1.0,g:0.0,b:0.0},
+                    {r:0.0,g:1.0,b:0.0},
+                    {r:0.0,g:0.0,b:1.0},
+                    {r:1.0,g:1.0,b:0.0},
+                    {r:1.0,g:1.0,b:1.0},
+                    {r:0.5,g:0.5,b:0.5},
+                    {r:0.1,g:0.0,b:0.0},
+                    {r:0.1,g:0.1,b:0.0},
+                    {r:0.9,g:0.0,b:0.0},
+                    {r:0.9,g:0.9,b:0.0},
+                ]
+                .forEach
+                (
+                    i => it(`hslToRgb(rgbToHsl(${JSON.stringify(i)}))`, () => assertDeepNearEqual(phiColors.hslToRgb(phiColors.rgbToHsl(i)),i))
+                )
         );
-        */
     }
 );
