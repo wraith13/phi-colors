bf4e109a5d2aae826bcabd578af8e66fe8d45cd3
道化師 <wraith13@hotmail.co.jp>
2019-03-03 19:29:28 +0900

    🦋 undefined を受け付ける前提だったのにインタフェース上受け付ける型指定になってなかったので修正

--- source/index.ts
+++ source/index.ts
@@ -245,7 +245,7 @@ export module phiColors
             a: Math.max(0.0, Math.min(1.0, expression.a)),
         }
     );
-    export const generate = (base: Hsla, h : number, s : number, l :number, a :number, isAlignLuma : boolean = true): Hsla =>
+    export const generate = (base: Hsla, h : number | undefined, s : number | undefined, l :number | undefined, a :number | undefined, isAlignLuma : boolean = true): Hsla =>
     {
         const hsla =
         {
